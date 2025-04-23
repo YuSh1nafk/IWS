a316ab83101a4fadfacc83f66cb262b7a2a6aa24
phakik <minhphuc.pn14.pn@gmail.com>
2025-04-23 14:38:17 +0700

    Thêm chức năng X

--- Server/index.js
+++ Server/index.js
@@ -1,22 +1,25 @@
 // index.js
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 
 import authRoutes from "./routes/authRoute.js";
 import productRoutes from "./routes/productRoutes.js";
 
 const app = express();
+dotenv.config(); 
 
 
 // Routes
+app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/products", productRoutes);
 // (sau này thêm cart, order...)
 
 const PORT = process.env.PORT || 5000;
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     app.listen(PORT, () => {
       console.log("✅ Server is running on port", PORT);
