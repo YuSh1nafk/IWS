8f53b8cdc24f3aee9d4fa401959ccf6fefaad753
phakik <minhphuc.pn14.pn@gmail.com>
2025-04-24 10:12:49 +0700

    Hoàn thiện giỏ hàng

---
+++ Server/models/cart.js
@@ -0,0 +1,17 @@
+// server/models/Cart.js
+import mongoose from "mongoose";
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    items: [
+      {
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+        quantity: { type: Number, default: 1 }
+      }
+    ]
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Cart", CartSchema);
